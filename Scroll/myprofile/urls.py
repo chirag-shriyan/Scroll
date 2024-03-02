bd469a1e16dd959f2b89fe44cc9bd6c18c341d7a
Chirag Shriyan <shubhamshiryan123@gmail.com>
2024-03-02 09:26:13 +0530

    User profile added

--- Scroll/myprofile/urls.py
+++ Scroll/myprofile/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('search/', views.Search ,name='search'),
+    path('profile/', views.User_Profile ,name='profile'),
 
 ]
--- Scroll/myprofile/views.py
+++ Scroll/myprofile/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from myauth.models import ProfilePic
+from posts.models import Post
 from django.db.models import Q
 from django.contrib.auth.decorators import login_required
 
@@ -34,3 +35,33 @@ def Search(req):
         return render(req,'search.html',context)
 
     return render(req,'search.html')
+
+@login_required(login_url = '/login')
+def User_Profile(req):
+
+    username = req.user.username.capitalize()
+    profile_pic = ProfilePic.objects.filter(user_id = req.user.id).values().first()
+    not_found = False
+
+    if profile_pic:
+        profile_pic = {"file": profile_pic['file'] , "exist": True}
+    else:
+        profile_pic = {"file": 'images/profile.jpg', "exist": False}
+
+    DATA = Post.objects.filter(user_id = req.user.id).values()
+
+    if not len(DATA) > 0:
+        not_found = True
+
+    context = {
+        "username": username,
+        "profile_pic": profile_pic,
+        "data": DATA,
+        "not_found": not_found
+    }
+
+    if req.method == 'POST':
+
+        return render(req,'user_profile.html',context)
+    else:
+        return render(req,'user_profile.html',context)
